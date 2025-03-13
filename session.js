43e3311e41735f9fc0f766e9e71273bfd5800132
tharuwa28 <120113674+tharuwa28@users.noreply.github.com>
2025-03-13 18:51:26 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,8 +40,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~WYthGA5S#AqCYzNAJtqcpAnYQGvpDjGufPH7nB_Zdt3xdqULLKlg",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~WIskmTJR#w4vUveJXHdbSZ11GUiqoGTDXxkeNS09MTuFf6LIb4-U",
+SUDO: process.env.SUDO || ["94762862143","94758775628"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+FOOTER: process.env.FOOTER || "> 𝙵𝙸𝙻𝙼 𝚆𝙾𝚁𝙻𝙳|𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 𝚃𝙷𝙰𝚁𝚄𝚆𝙰.",
 };
